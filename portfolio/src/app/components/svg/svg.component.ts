26c0e89322d2740b2cdf33fb87033b9b7f1e1c9c
Amanda Simonds <amandasimonds9@gmail.com>
2023-03-24 16:29:02 -0400

    svg component, its a bit hacky but it will work for now :p

---
+++ portfolio/src/app/components/svg/svg.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input, TemplateRef } from '@angular/core'
+
+@Component({
+    selector: 'app-svg',
+    templateUrl: './svg.component.svg',
+    styleUrls: ['./svg.component.scss'],
+})
+export class SvgComponent {
+  @Input() public svg = '';
+  @Input() public color = '';
+}
