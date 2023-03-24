9ef09977b82bf3bcda23b337fb08c2348b6d2a31
Amanda Simonds <amandasimonds9@gmail.com>
2023-03-24 17:00:26 -0400

    changed svg template to html file, added svgs to svg component switch, added styling to svgs, added nav icons

--- portfolio/src/app/components/svg/svg.component.ts
+++ portfolio/src/app/components/svg/svg.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input, TemplateRef } from '@angular/core'
+import { Component, Input } from '@angular/core'
 
 @Component({
     selector: 'app-svg',
-    templateUrl: './svg.component.svg',
+    templateUrl: './svg.component.html',
     styleUrls: ['./svg.component.scss'],
 })
 export class SvgComponent {
