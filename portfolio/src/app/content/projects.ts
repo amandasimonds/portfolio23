e0b0d876ce8a2e75a2c1942d2894f7d9e7dad3aa
Amanda Simonds <amandasimonds9@gmail.com>
2023-05-19 18:50:12 -0400

    added picture and links for data-platform-prototype project, style and copy edit

--- portfolio/src/app/content/projects.ts
+++ portfolio/src/app/content/projects.ts
@@ -1,9 +1,9 @@
 export const Projects = [
     {
         title: 'Platform Prototype',
-        github: 'https://github.com/amandasimonds',
-        link: 'https://amandaxandra.netlify.app/',
-        img: 'https://res.cloudinary.com/dr0uztw3o/image/upload/v1683134405/portfolio-screenshot_fa6i9q.png',
+        github: 'https://github.com/amandasimonds/data-platform-prototype-1',
+        link: 'https://amandasimonds.github.io/data-platform-prototype-1',
+        img: 'https://res.cloudinary.com/dr0uztw3o/image/upload/v1684535938/data-platform-prototype_v0umd9.png',
         description: 'High fidelity prototype of a data platform application. I built this prototype for testing usability and workflow with the design team. It was also used by Product Owners and others in the organization as a tool for showing the end-goal of the huge data platform our team was working towards. This project marks a turning point in my development as a front-end engineer, and the value I aim to bring to the future teams I join.',
         tech: [
             'Angular',
@@ -22,6 +22,18 @@ export const Projects = [
             'Typescript',
             'SCSS',
             'Storybook'
+        ]
+    },
+    {
+        title: 'Stream Ed',
+        github: 'https://github.com/amandasimonds/stream-ed',
+        link: 'https://amandasimonds.github.io/stream-ed/#/',
+        img: 'https://res.cloudinary.com/dr0uztw3o/image/upload/v1683134405/portfolio-screenshot_fa6i9q.png',
+        description: 'App for Remote Education',
+        tech: [
+            'React',
+            'JavaScript',
+            'SCSS',
         ]
     }
 ]
