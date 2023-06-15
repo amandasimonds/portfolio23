3608012d8bb5f782e24dbfce8e5ee774925e3fb9
Amanda Simonds <amandasimonds9@gmail.com>
2023-06-15 17:44:19 -0400

    added dates to work experience

--- portfolio/src/app/content/experience.ts
+++ portfolio/src/app/content/experience.ts
@@ -3,7 +3,7 @@ export const Experience = [
         title: 'UI Engineer',
         company: 'IHS Markit/S&P Global',
         companyLink: 'https://www.spglobal.com/en/',
-        date: 'May 2021 - Present',
+        date: 'May 2021 - May 2023',
         duties: [
             'Developed the UI Component library for a large data platform app. Using Angular and Storybook, I led the organization of a collaborative process for all developers to contribute to the library.',
             'Worked with designers and product owners to create a high fidelity prototype of the application, to use for presenting user flow ideas, testing usability, and hands on testing for the design process',
@@ -15,7 +15,7 @@ export const Experience = [
         title: 'SQA Engineer',
         company: 'IHS Markit',
         companyLink: 'https://news.ihsmarkit.com/',
-        date: 'October 2020 - May 2021',
+        date: 'Oct 2020 - May 2021',
         duties: [
             'Researched in greenfield development for developing a large data platform application, including research of Amazon Web Services and Terraform',
             'Developed infrastructure-as-code structures for setting up S3, Authentication, and testing',
@@ -26,7 +26,7 @@ export const Experience = [
         title: 'Code Sensei',
         company: 'Code Ninjas',
         companyLink: 'https://codeninjas.com/',
-        date: 'July 2020 - October 2020',
+        date: 'July 2020 - Oct 2020',
         duties: [
             'Provide students with coding problems on and off the curriculum that challenge their problem solving skills',
             'Innovate new coding challenges and instruct on Javascript, HTML, CSS, and Python coding',
